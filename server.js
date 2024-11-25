5babae8305e3963969583921f5bff5a50aa42ede
jackson1002 <102551009+jackson1002@users.noreply.github.com>
2024-11-25 11:06:44 -0300

    atualizei

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express()
 const PORT = 3000;
 
 
-//middleware para ler as requisições HTTP que vierem dp front
+//middleware para ler as requisições HTTP que vierem do front
 app.use(bodyParser.json())
 
 //Rotas(Isso aqui é o endereço que você vai chamar no front,isso o que vai ser executado)
 app.use('./api/auth', authRoutes);
 
 app.listen(PORT, ()=>{
-    console.log(`Servidor rodando na porta ${PORT}`)
+    console.log(`Servidor rodando na porta http://localhost:3000`) //Antes era console.log(`Servidor rodando na porta ${PORT}`)mostrando essa mensagem no terminal,mostrando a porta 3000  
 })
